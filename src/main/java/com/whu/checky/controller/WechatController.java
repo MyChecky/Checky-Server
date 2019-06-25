4234aac638810a49df000daadd0a1198d033b981
Scott <yeunghl@whu.edu.cn>
2019-06-25 21:39:37 +0800

    try impl login function

--- src/main/java/com/whu/checky/controller/WechatController.java
+++ src/main/java/com/whu/checky/controller/WechatController.java
@@ -1,12 +1,67 @@
 package com.whu.checky.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.whu.checky.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.UUID;
 
 @RestController
 public class WechatController {
 
-    void login(){
+    private String wxspAppid="";
+    private String wxspSecret="";
 
+    @Autowired
+    ObjectMapper mapper;
+
+    @PostMapping("/login")
+    public User login(@RequestParam String code) throws IOException {
+        RestTemplate restTemplate = new RestTemplate();// 发送request请求
+        String params = "appid=" + wxspAppid + "&secret=" + wxspSecret + "&js_code=" +code;//参数
+        String url = "https://api.weixin.qq.com/sns/jscode2session?"+params;// 微信接口 用于查询oponid
+        String response = restTemplate.getForObject(url,String.class);
+
+
+
+        JsonNode node = this.mapper.readTree(response);
+        String openid = node.get("opendid").asText();
+        String sessionKey = node.get("session_key").asText();
+
+
+//        logger.info("response:"+response);
+//        WeixinRespense weixinRespense = objectMapper.readValue(response,WeixinRespense.class); // 逆序列化 ，将字符串中的有效信息取出
+//        String session_key = weixinRespense.getSession_key();//如果解密encryptData获取unionId，会用的到
+//        openid= weixinRespense.getOpenid();//微信小程序 用户唯一标识
+//        logger.info("session_key:"+session_key);
+//        logger.info("openid:"+openid);
+
+        // 注册用户，将查询到的oponid作为id
+        User user = new User();
+        String skey = UUID.randomUUID().toString();
+        user.setSessionID(skey);
+//        user.setUid(openid);
+//        user.setBalence(Float.valueOf(0));
+//        if (userService.existbyid(openid))
+//        {
+//            return userService.getUserById(openid);
+//        }
+//        else {
+//            if (userService.signupUser(user) == 1)
+//            {
+//                return userService.getUserById(openid);
+//            }
+//            else {
+//                return null;
+//            }
+//        }
+        return user;
     }
 
 
--- src/main/java/com/whu/checky/domain/User.java
+++ src/main/java/com/whu/checky/domain/User.java
@@ -1,28 +1,32 @@
 package com.whu.checky.domain;
 
-import org.springframework.data.annotation.Id;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
+
 import java.sql.Date;
 
 public class User {
     //用户id
-    @Id
-    @GeneratedValue
-    private int userid;
-    //用户微信名
-    @Column(nullable = false)
-    private String nickname;
-    //用户微信头像url
-    @Column(nullable = false)
-    private String avatar;
-    //用户性别
-    @Column(nullable = false)
-    private String gender;
-    //用户使用小程序的时间
-    @Column(nullable = false)
-    private Date signindate;
-    //地理位置
+//    @Id
+//    @GeneratedValue
+//    private int userid;
+//    //用户微信名
+//    @Column(nullable = false)
+//    private String nickname;
+//    //用户微信头像url
+//    @Column(nullable = false)
+//    private String avatar;
+//    //用户性别
+//    @Column(nullable = false)
+//    private String gender;
+//    //用户使用小程序的时间
+//    @Column(nullable = false)
+//    private Date signindate;
+//    //地理位置
+
+    private String sessionID;
+
+    public void setSessionID(String id){
+        sessionID=id;
+    }
 
 }
