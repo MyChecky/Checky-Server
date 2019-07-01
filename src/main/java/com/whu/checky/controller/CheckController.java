d20d2ee2e4224fe2c479de0816767f14e736338f
nzxxavier <15927400635@163.com>
2019-07-01 14:49:05 +0800

    实现增删改查

--- src/main/java/com/whu/checky/controller/CheckController.java
+++ src/main/java/com/whu/checky/controller/CheckController.java
@@ -2,9 +2,7 @@ package com.whu.checky.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.whu.checky.domain.Administrator;
 import com.whu.checky.domain.Check;
-import com.whu.checky.service.AdministratorService;
 import com.whu.checky.service.CheckService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,8 +19,8 @@ public class CheckController {
 
     @PostMapping("/Checky/check/addCheck")
     public String addCheck(@RequestBody String body){
-
-        Check check = paserJson2User(body);
+        JSONObject object = JSONObject.parseObject(body);
+        Check check = paserJson2NewCheck(object);
 
         try{
             checkService.addCheck(check);
@@ -36,24 +34,53 @@ public class CheckController {
 
     @PostMapping("/Checky/check/queryCheck")
     public Check queryCheck(@RequestBody String body){
-        String checkId= (String) JSON.parse(body);
-        List<Check> list = checkService.queryCheck(checkId);
-        if(list.size()==1){
+        String checkId = (String) JSON.parse(body);
+        List<Check> list = checkService.queryCheck("check_id", checkId);
+        if(list.size()==1)
             return list.get(0);
-        }
         else
             return null;
     }
 
-    void updateCheck(){
-
+    @PostMapping("/Checky/check/listCheck")
+    public List<Check> listCheck(@RequestBody String body){
+        String userId = (String) JSON.parse(body);
+        return checkService.queryCheck("user_id", userId);
     }
 
-    void deleteCheck(){}
-
-    private Check paserJson2User(String body){
-        //解析json获取登录信息
+    @PostMapping("/Checky/check/updateCheck")
+    public String updateCheck(@RequestBody String body){
         JSONObject object = JSONObject.parseObject(body);
+        Check check = paserJasonUpdateUser(object);
+
+        try{
+            checkService.updateCheck(check);
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+            return "fail";
+        }
+
+        return "success";
+    }
+
+    @PostMapping("/Checky/check/deleteCheck")
+    public String deleteCheck(@RequestBody String body){
+        String checkId = (String) JSON.parse(body);
+
+        try{
+            checkService.deleteCheck(checkId);
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+            return "fail";
+        }
+
+        return "success";
+    }
+
+    private Check paserJson2NewCheck(JSONObject object){
+        //解析json获取登录信息
         String userId = object.getString("userId");
         String taskId = object.getString("taskId");
         String checkTime = object.getString("checkTime");
@@ -65,6 +92,16 @@ public class CheckController {
         check.setUserId(userId);
         check.setTaskId(taskId);
         check.setCheckTime(checkTime);
+
+        return check;
+    }
+
+    private Check paserJasonUpdateUser(JSONObject object){
+        Check check = paserJson2NewCheck(object);
+        check.setCheckState(object.getString("checkState"));
+        check.setCheckTime(object.getString("checkTime"));
+        check.setPassNum(object.getInteger("passNum"));
+        check.setSuperviseNum(object.getInteger("superviseNum"));
 
         return check;
     }
