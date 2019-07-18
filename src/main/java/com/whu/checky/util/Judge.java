311736524605071ee8e50240c655303041fe06a9
Scott <yeunghl@whu.edu.cn>
2019-07-18 11:28:56 +0800

    完成任务结算中的资金结算部分

--- src/main/java/com/whu/checky/util/Judge.java
+++ src/main/java/com/whu/checky/util/Judge.java
@@ -1,12 +1,9 @@
 package com.whu.checky.util;
 
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
-import com.whu.checky.domain.Check;
-import com.whu.checky.domain.Supervise;
-import com.whu.checky.domain.Task;
-import com.whu.checky.mapper.CheckMapper;
-import com.whu.checky.mapper.SuperviseMapper;
-import com.whu.checky.mapper.TaskMapper;
+import com.whu.checky.domain.*;
+import com.whu.checky.mapper.*;
+import com.whu.checky.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -14,8 +11,7 @@ import org.springframework.stereotype.Component;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 //仲裁模块
 @Component
@@ -35,6 +31,15 @@ public class Judge {
 
     @Autowired
     SuperviseMapper superviseMapper;
+
+    @Autowired
+    TaskService taskService;
+
+    @Autowired
+    MoneyFlowMapper moneyFlowMapper;
+
+    @Autowired
+    UserMapper userMapper;
 
     SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
@@ -65,7 +70,7 @@ public class Judge {
                 e.printStackTrace();
             }
         }
-
+    }
 //            List<Supervise> superviseList = superviseMapper.selectList(new EntityWrapper<Supervise>()
 //                .eq("check_id",c.getCheckId())
 //            );
@@ -97,9 +102,10 @@ public class Judge {
 //            } catch (ParseException e) {
 //                e.printStackTrace();
 //            }
-        }
+
 
     @Scheduled(cron = "${jobs.judge.task.cron}")
+//    @Scheduled(initialDelay = 0,fixedRate = 1000000000)
     public void taskFinishJudge(){
         Date today = new Date();
         String now = sdf.format(today);
@@ -116,9 +122,45 @@ public class Judge {
                 .and()
                 .eq("task_id",t.getTaskId())
             );
-            if(count>=t.getCheckTimes()) t.setTaskState("success");
-            else if(count-timeoutDay<=t.getCheckTimes()) t.setTaskState("fail");
+            if(count>=t.getCheckTimes()) {
+                t.setTaskState("success");
+                //成功全额退款
+                MoneyFlow record = new MoneyFlow();
+                record.setFlowId(UUID.randomUUID().toString());
+                record.setFlowMoney(t.getTaskMoney());
+                record.setFlowTime(sdf.format(new Date()));
+                record.setFromUserId("system");
+                record.setToUserId(t.getUserId());
+                moneyFlowMapper.insert(record);
+                //更新账户余额
+                User user = userMapper.selectById(t.getUserId());
+                user.setUserMoney(user.getUserMoney()+t.getTaskMoney());
+                userMapper.updateById(user);
+            }
+            else if(count-timeoutDay<=t.getCheckTimes()) {
+                t.setTaskState("fail");
+
+                //失败全部分成
+                HashMap<String,Double> distribute = taskService.distribute(t.getTaskId());
+                for(Map.Entry<String,Double> pair:distribute.entrySet()){
+
+                    MoneyFlow record = new MoneyFlow();
+                    record.setFlowId(UUID.randomUUID().toString());
+                    record.setFlowMoney(pair.getValue());
+                    record.setFlowTime(sdf.format(new Date()));
+                    record.setFromUserId("system");
+                    record.setToUserId(pair.getKey());
+                    moneyFlowMapper.insert(record);
+
+                    User user = userMapper.selectById(pair.getKey());
+                    user.setUserMoney(user.getUserMoney()+pair.getValue());
+                    userMapper.updateById(user);
+                }
+            }
             else continue;
+
+
+
             taskMapper.updateById(t);
         }
     }
