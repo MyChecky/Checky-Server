0962e37bf2f214c9a32de3636dc79db874d471ae
Scott <yeunghl@whu.edu.cn>
2019-07-18 14:55:24 +0800

    记录清空数据库所有表的脚本

---
+++ src/main/resources/delAllData.sql
@@ -0,0 +1,19 @@
+SET foreign_key_checks = 0;
+
+truncate TABLE checky.administrator;
+truncate TABLE checky.appeal;
+truncate TABLE checky.check;
+truncate TABLE checky.comment;
+truncate TABLE checky.essay;
+truncate TABLE checky.moneyflow;
+truncate TABLE checky.record;
+truncate TABLE checky.report;
+truncate TABLE checky.suggestion;
+truncate TABLE checky.supervise;
+truncate TABLE checky.task;
+truncate TABLE checky.task_supervisor;
+truncate TABLE checky.task_type;
+truncate TABLE checky.user;
+truncate TABLE checky.user_friend;
+
+SET foreign_key_checks = 1;
