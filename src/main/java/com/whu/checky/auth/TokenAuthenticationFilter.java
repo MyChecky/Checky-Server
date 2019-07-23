dad71224ca4a810be94ab59fadee8881a9eb6bb0
Scott <yeunghl@whu.edu.cn>
2019-07-23 22:41:08 +0800

    修改身份认证消息获取方式

--- src/main/java/com/whu/checky/auth/TokenAuthenticationFilter.java
+++ src/main/java/com/whu/checky/auth/TokenAuthenticationFilter.java
@@ -11,14 +11,10 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
-import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.Map;
 
 /**
  * Grab the `?token=xxx` parameter into authentication, so as to trigger spring security's
@@ -37,35 +33,37 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
             // do nothing
         } else {
 
-            BufferedReader reader = req.getReader();
-            StringBuilder builder = new StringBuilder();
+//            从请求体中获取相关认证参数，弃用改为从请求首部获取
+//            BufferedReader reader = req.getReader();
+//            StringBuilder builder = new StringBuilder();
+//
+//            String line;
+//            while ((line = reader.readLine()) != null) {
+//                builder.append(line);
+//            }
+//            String json = builder.toString();
+//            if (json.equals("")) {
+//                fc.doFilter(req, res);
+//                return;
+//            }
+//
+////            int len = req.getContentLength();
+////            ServletInputStream iii = req.getInputStream();
+////            byte[] buffer = new byte[len];
+////            iii.read(buffer, 0, len);
+////            String json = new String(buffer);
+//            JSONObject data = JSON.parseObject(json);
 
-            String line;
-            while((line = reader.readLine()) != null) {
-                builder.append(line);
-            }
-            String json = builder.toString();
-            if(json.equals("")) {
-                fc.doFilter(req, res);
-                return;
+
+            String sessionKey = req.getHeader("sessionKey");
+            String userId = req.getHeader("userId");
+
+
+            if (sessionKey != null && userId != null) {
+                Authentication auth = new TokenAuthentication(sessionKey, userId);
+                SecurityContextHolder.getContext().setAuthentication(auth);
             }
 
-//            int len = req.getContentLength();
-//            ServletInputStream iii = req.getInputStream();
-//            byte[] buffer = new byte[len];
-//            iii.read(buffer, 0, len);
-//            String json = new String(buffer);
-            JSONObject data = JSON.parseObject(json);
-
-//            Map<String, String[]> params = req.getParameterMap();
-            if (data.containsKey("sessionKey") && data.containsKey("userId")) {
-                String token = data.getString("sessionKey");
-                String userId = data.getString("userId");
-                if (token != null && userId != null) {
-                    Authentication auth = new TokenAuthentication(token,userId);
-                    SecurityContextHolder.getContext().setAuthentication(auth);
-                }
-            }
             req.setAttribute("com.whu.checky.auth.TokenAuthenticationFilter.FILTERED", true);
         }
 
@@ -94,7 +92,7 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
 
         @Override
         public Object getCredentials() {
-            return new Pair<>(token,userId);
+            return new Pair<>(token, userId);
         }
 
         @Override
