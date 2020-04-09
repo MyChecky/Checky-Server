cb415bd4dc00758bc3c20c1257dc24609a2795c0
lugf027 <161230578@qq.com>
2020-04-09 16:10:24 +0800

    war时改成这样

--- src/main/java/com/whu/checky/config/WebSocketConfig.java
+++ src/main/java/com/whu/checky/config/WebSocketConfig.java
@@ -6,8 +6,10 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
 public class WebSocketConfig {
-    @Bean
-    public ServerEndpointExporter serverEndpointExporter() {
-        return new ServerEndpointExporter();
-    }
+    // 使用内置tomcat（/调试）时，不注释；部署war包放到服务器的tomcat下时，注释掉
+    // 评论区：https://blog.csdn.net/kxj19980524/article/details/88751114
+//    @Bean
+//    public ServerEndpointExporter serverEndpointExporter() {
+//        return new ServerEndpointExporter();
+//    }
 }
