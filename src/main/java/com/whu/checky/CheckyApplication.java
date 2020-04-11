426e831d68452c50e91036f6b97ac4cc92e19d29
hwenliu <37737892@qq.com>
2020-04-11 10:18:06 +0800

    1.先按时恢复jar打包模式

--- src/main/java/com/whu/checky/CheckyApplication.java
+++ src/main/java/com/whu/checky/CheckyApplication.java
@@ -10,14 +10,16 @@ import org.springframework.stereotype.Component;
 @SpringBootApplication
 @EnableScheduling
 // 这个文件不改的话，添加war app一直不启动springboot这个项目，接口也是404
-public class CheckyApplication extends SpringBootServletInitializer {
-    //    public class CheckyApplication{
+//public class CheckyApplication extends SpringBootServletInitializer {
+public class CheckyApplication{
     public static void main(String[] args) {
         SpringApplication.run(CheckyApplication.class, args);
     }
 
+    /*
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(CheckyApplication.class);
     }
+    */
 }
--- src/main/java/com/whu/checky/config/WebSocketConfig.java
+++ src/main/java/com/whu/checky/config/WebSocketConfig.java
@@ -8,8 +8,8 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 public class WebSocketConfig {
     // 使用内置tomcat（/调试）时，不注释；部署war包放到服务器的tomcat下时，注释掉
     // 评论区：https://blog.csdn.net/kxj19980524/article/details/88751114
-//    @Bean
-//    public ServerEndpointExporter serverEndpointExporter() {
-//        return new ServerEndpointExporter();
-//    }
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
 }
