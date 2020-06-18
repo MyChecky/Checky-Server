f42e0c9a7523fcf2f96dfd12af3afb4183ab14c1
lugf027 <1606962606@qq.com>
2020-06-18 23:11:01 +0800

    http8090+https443
    应该可以用了，我把www.ycloudtech.cn强制解析到本机127.0.0.1测试亦是可以访问的

--- src/main/java/com/whu/checky/CheckyApplication.java
+++ src/main/java/com/whu/checky/CheckyApplication.java
@@ -1,11 +1,15 @@
 package com.whu.checky;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @EnableScheduling
@@ -16,7 +20,32 @@ public class CheckyApplication{
         SpringApplication.run(CheckyApplication.class, args);
     }
 
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                collection.addMethod("post");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
 
+    private Connector redirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8090);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
+    }
 
     /*
     @Bean
