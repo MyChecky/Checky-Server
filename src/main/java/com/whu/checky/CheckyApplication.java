93af2fc58ad2090e0af2ca1709c702dbdd4668a5
lugf027 <1606962606@qq.com>
2020-06-19 11:08:41 +0800

    read port from SQL

--- src/main/java/com/whu/checky/CheckyApplication.java
+++ src/main/java/com/whu/checky/CheckyApplication.java
@@ -1,12 +1,15 @@
 package com.whu.checky;
 
+import com.whu.checky.service.ParameterService;
 import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.Ssl;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -16,13 +19,17 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 // 这个文件不改的话，添加war app一直不启动springboot这个项目，接口也是404
 //public class CheckyApplication extends SpringBootServletInitializer {
 public class CheckyApplication{
+    @Autowired
+    private ParameterService parameterService;
+
     public static void main(String[] args) {
         SpringApplication.run(CheckyApplication.class, args);
     }
 
     @Bean
     public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+        int httpsPort = Integer.parseInt(parameterService.getValueByParam("https").getParamValue());
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory(httpsPort) {
             @Override
             protected void postProcessContext(Context context) {
                 SecurityConstraint securityConstraint = new SecurityConstraint();
@@ -34,16 +41,17 @@ public class CheckyApplication{
                 context.addConstraint(securityConstraint);
             }
         };
-        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        tomcat.addAdditionalTomcatConnectors( redirectConnector(httpsPort));
         return tomcat;
     }
 
-    private Connector redirectConnector() {
+    private Connector redirectConnector(int httpsPort) {
+        int httpPort = Integer.parseInt(parameterService.getValueByParam("http").getParamValue());
         Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
         connector.setScheme("http");
-        connector.setPort(8090);
+        connector.setPort(httpPort);
         connector.setSecure(false);
-        connector.setRedirectPort(443);
+        connector.setRedirectPort(httpsPort);
         return connector;
     }
 
