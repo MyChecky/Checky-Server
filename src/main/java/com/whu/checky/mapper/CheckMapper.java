5be680b88196e53734506b544bdb85ab651bd313
lugf027 <161230578@qq.com>
2020-11-15 21:09:26 +0800

    接上 Medal

--- src/main/java/com/whu/checky/mapper/CheckMapper.java
+++ src/main/java/com/whu/checky/mapper/CheckMapper.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.whu.checky.domain.Check;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 
 @Mapper
@@ -12,4 +13,12 @@ public interface CheckMapper extends BaseMapper<Check> {
     void insertNewCheck(Check check);
     void updatePassSuperviseCheck(@Param("checkId") String checkId);
     void updateDenySuperviseCheck(@Param("checkId") String checkId);
+
+    @Select("SELECT COUNT(*) \n" +
+            "FROM `CHECK` \n" +
+            "WHERE (\n" +
+            "`USER_ID` = \"#{userId}\" \n" +
+            "AND `CHECK_STATE` = \"#{checkState}\"\n" +
+            ")")
+    int countStateNumberForSomeone(@Param("userId") String userId, @Param("checkState") String checkState);
 }
