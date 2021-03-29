bb3bd3f63666a61becfdff8e34ad219e44476f99
37737892@qq.com <37737892@qq.com>
2021-03-29 11:47:25 +0800

    1.https 访问端口8090,http访问端口8099

--- src/main/java/com/whu/checky/config/WxPayConfig.java
+++ src/main/java/com/whu/checky/config/WxPayConfig.java
@@ -15,15 +15,13 @@ public class WxPayConfig {
     //小程序密钥
     public static final String secret = "";
     //支付成功后的服务器回调url
-    public static final String notifyUrl = "https:127.0.0.1:8080/money/notify";//待修改，目前在本地调试
+    public static final String notifyUrl = "https://127.0.0.1:8080/money/notify";
     //交易类型，小程序支付的固定值为JSAPI
     public static final String tradeType = "JSAPI";
     //签名方式，固定值
     public static final String signType = "MD5";
-    //统一订单接口url地址(沙盒)
+    //统一订单接口url地址
     public static final String createOrderUrl = "https://api.mch.weixin.qq.com/sandboxnew/pay/unifiedorder";
     //沙箱测试key
     public static final String getSandBoxSignKeyUrl = "https://api.mch.weixin.qq.com/sandboxnew/pay/getsignkey";
-    //统一订单接口url地址
-    public static final String unifiedOrderUrl="https://api.mch.weixin.qq.com/pay/unifiedorder";
 }
